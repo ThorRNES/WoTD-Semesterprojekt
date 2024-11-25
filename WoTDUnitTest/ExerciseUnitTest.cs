82d1a7fd46409e7cbf7ce48cc10ad65826e259be
MurtSkytte <mart175b@edu.zealand.dk>
2024-11-25 14:14:21 +0100

    Unit Tests er blevet tilføjet

---
+++ WoTDUnitTest/ExerciseUnitTest.cs
@@ -0,0 +1,67 @@
+using WorkOutToDO.Models;
+
+namespace WoTDUnitTest
+{
+    [TestClass]
+    public class ExerciseUnitTest
+    {
+        private Exercise exerciseTrue = new Exercise { Name = "Incline Hammercurls", Type = "Strength", Muscle = "Strength", Equipment = "Dumpbell", Difficulty = "Beginner", Instructions = "Seat yourself..." };
+        private Exercise exerciseNameNull = new Exercise { Name = null, Type = "Strength", Muscle = "Strength", Equipment = "Dumpbell", Difficulty = "Beginner", Instructions = "Seat yourself..." };
+        private Exercise exerciseTypeNull = new Exercise { Name = "Incline Hammercurls", Type = null, Muscle = "Strength", Equipment = "Dumpbell", Difficulty = "Beginner", Instructions = "Seat yourself..." };
+        private Exercise exerciseMuscleNull = new Exercise { Name = "Incline Hammercurls", Type = "Strength", Muscle = null, Equipment = "Dumpbell", Difficulty = "Beginner", Instructions = "Seat yourself..." };
+        private Exercise exerciseEquipNull = new Exercise { Name = "Incline Hammercurls", Type = "Strength", Muscle = "Strength", Equipment = null, Difficulty = "Beginner", Instructions = "Seat yourself..." };
+        private Exercise exerciseDiffiNull = new Exercise { Name = "Incline Hammercurls", Type = "Strength", Muscle = "Strength", Equipment = "Dumpbell", Difficulty = null, Instructions = "Seat yourself..." };
+        private Exercise exerciseInstruNull = new Exercise { Name = "Incline Hammercurls", Type = "Strength", Muscle = "Strength", Equipment = "Dumpbell", Difficulty = "Beginner", Instructions = null };
+
+
+
+        [TestMethod]
+
+        public void ValidateValidate()
+        {
+
+        }
+        public void ValidateNameTest()
+        {
+            exerciseTrue.ValidateName();
+            Assert.ThrowsException<ArgumentNullException>(() => exerciseNameNull.ValidateName());
+
+        }
+
+        [TestMethod]
+        public void ValidateTypeTest()
+        {
+            exerciseTrue.ValidateType();
+            Assert.ThrowsException<ArgumentNullException>(() => exerciseTypeNull.ValidateType());
+        }
+        [TestMethod]
+        public void ValidateMuscleTest()
+        {
+            exerciseTrue.ValidateMuscle();
+            Assert.ThrowsException<ArgumentNullException>(() => exerciseMuscleNull.ValidateMuscle());
+        }
+        [TestMethod]
+        public void ValidateEquipmentTest()
+        {
+            exerciseTrue.ValidateEquipment();
+            Assert.ThrowsException<ArgumentNullException>(() => exerciseEquipNull.ValidateEquipment());
+        }
+        [TestMethod]
+        public void ValidateDifficultyTest()
+        {
+            exerciseTrue.ValidateDifficulty();
+            Assert.ThrowsException<ArgumentNullException>(() => exerciseDiffiNull.ValidateDifficulty());
+        }
+        [TestMethod]
+        public void ValidateInstructionsTest()
+        {
+            exerciseTrue.ValidateInstructions();
+            Assert.ThrowsException<ArgumentNullException>(() => exerciseInstruNull.ValidateInstructions());
+        }
+        [TestMethod]
+        public void ValidateTest()
+        {
+            exerciseTrue.Validate();
+        }
+    }
+}
---
+++ WoTDUnitTest/PersonUnitTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkOutToDO.Models;
+
+namespace WoTDUnitTest
+{
+    [TestClass]
+    public class PersonUnitTest
+    {
+        private Person personTrue = new Person { FName = "Thor Russel", Gender = "Mand", Age = 25, AvgPulse = 125, Weight = 65, Height = 172 };
+        private Person personNameNull = new Person { FName = null, Gender = "Mand", Age = 25, AvgPulse = 125, Weight = 65, Height = 172 };
+        private Person personGenderNull = new Person { FName = "Thor Russel", Gender = null, Age = 25, AvgPulse = 125, Weight = 65, Height = 172 };
+        private Person personAgeNull = new Person { FName = "Thor Russel", Gender = "Mand", Age = null, AvgPulse = 125, Weight = 65, Height = 172 };
+        private Person personAgeNeg = new Person { FName = "Thor Russel", Gender = "Mand", Age = -25, AvgPulse = 125, Weight = 65, Height = 172 };
+        private Person personPulseNull = new Person { FName = "Thor Russel", Gender = "Mand", Age = 25, AvgPulse = null, Weight = 65, Height = 172 };
+        private Person personPulseNeg = new Person { FName = "Thor Russel", Gender = "Mand", Age = 25, AvgPulse = -125, Weight = 65, Height = 172 };
+        private Person personWeightNull = new Person { FName = "Thor Russel", Gender = "Mand", Age = 25, AvgPulse = 125, Weight = null, Height = 172 };
+        private Person personWeightNeg = new Person { FName = "Thor Russel", Gender = "Mand", Age = 25, AvgPulse = 125, Weight = -65, Height = 172 };
+        private Person personHeightNull = new Person { FName = "Thor Russel", Gender = "Mand", Age = 25, AvgPulse = 125, Weight = 65, Height = null };
+        private Person personHeightNeg = new Person { FName = "Thor Russel", Gender = "Mand", Age = 25, AvgPulse = 125, Weight = 65, Height = -172 };
+
+        [TestMethod]
+        public void ValidateFNameTest()
+        {
+            personTrue.ValidateFName();
+            Assert.ThrowsException<ArgumentNullException>(() => personNameNull.ValidateFName());
+        }
+
+        [TestMethod]
+        public void ValidateGenderTest()
+        {
+            personTrue.ValidateGender();
+            Assert.ThrowsException<ArgumentNullException>(() => personGenderNull.ValidateGender());
+        }
+        [TestMethod]
+        public void ValidateAgeTest()
+        {
+            personTrue.ValidateAge();
+            Assert.ThrowsException<ArgumentNullException>(() => personAgeNull.ValidateAge());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => personAgeNeg.ValidateAge());
+        }
+        [TestMethod]
+        public void ValidateAvgPulseTest()
+        {
+            personTrue.ValidateAvgPulse();
+            Assert.ThrowsException<ArgumentNullException>(() => personPulseNull.ValidateAvgPulse());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => personPulseNeg.ValidateAvgPulse());
+        }
+        [TestMethod]
+        public void ValidateWeightTest()
+        {
+            personTrue.ValidateWeight();
+            Assert.ThrowsException<ArgumentNullException>(() => personWeightNull.ValidateWeight());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => personWeightNeg.ValidateWeight());
+        }
+        [TestMethod]
+        public void ValidateHeightTest()
+        {
+            personTrue.ValidateHeight();
+            Assert.ThrowsException<ArgumentNullException>(() => personHeightNull.ValidateHeight());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => personHeightNeg.ValidateHeight());
+        }
+        [TestMethod]
+        public void ValidateTest()
+        {
+            personTrue.Validate();
+        }
+    }
+}
