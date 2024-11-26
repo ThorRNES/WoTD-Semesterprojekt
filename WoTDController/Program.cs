8e240aab30a38e973f04202845e2b99cd89aaf8b
farda0007 <ardaumutyildiz@gmail.com>
2024-11-26 11:24:45 +0100

    Controller tilføjet, og ændringer i program

---
+++ WoTDController/Program.cs
@@ -0,0 +1,21 @@
+using WorkOutToDO.Models;
+using WorkOutToDO.Repos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<Func<Person, int>>(provider => person => person.Id);
+builder.Services.AddSingleton<GenericRepo<Person>>();
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
