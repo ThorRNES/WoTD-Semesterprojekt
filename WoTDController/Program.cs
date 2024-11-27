d4857e7261cd1038da36b7bad960035b02c6b0ad
Lucasloa <150661710+Lucasloa@users.noreply.github.com>
2024-11-27 12:39:44 +0100

    cors

--- WoTDController/Program.cs
+++ WoTDController/Program.cs
@@ -5,8 +5,15 @@ using WoTD_Semesterprojekt.Repos;
 var builder = WebApplication.CreateBuilder(args);
 
 
-
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+    );
+});
 builder.Services.AddControllers();
 builder.Services.AddSingleton<PersonRepo>(new PersonRepo());
 var app = builder.Build();
